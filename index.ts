6e494bb7d0265be93af1c46c447f62c5ffcda860
dg12988 <doug.goldberg@me.com>
2021-08-10 10:25:37 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,2 +1,21 @@
+import { Observable } from "rxjs";
 
+
+interface NewsItem{
+  category: 'Business' | 'Sports';
+  content: string;
+}
+
+const newsFeed$ = new Observable<NewsItem>(subscriber => {
+  setTimeout(() => 
+  subscriber.next({ category: "Business", content: 'A'}), 1000);
+  setTimeout(() => 
+  subscriber.next({ category: "Sports", content: 'B'}), 3000);
+  setTimeout(() => 
+  subscriber.next({ category: "Business", content: 'C'}), 4000);
+  setTimeout(() => 
+  subscriber.next({ category: "Sports", content: 'D'}), 6000);
+  setTimeout(() => 
+  subscriber.next({ category: "Business", content: 'E'}), 7000);
+});
 
